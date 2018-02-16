365b411672440e71e7d9ae6f066da05301967ba8
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 03:49:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,6 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  #return false if board.all?{ |e| e == " "}
   WIN_COMBINATIONS.detect do |combination|
     board[combination[0]] == board[combination[1]] &&
     board[combination[1]] == board[combination[2]] &&
